6bfb02ac4b10715d033777d016ea13d15b74d138
Dejan-Stojkoski <dejan_pp2005@live.com>
2021-03-24 11:05:52 +0100

    Homework Class 1 upload

---
+++ Class 1/Solution1/Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\n\n    *\n   ***\n  *****\n *******\n*********");
+
+            Console.WriteLine("\n\n   *\n   *\n * * *\n*  *  *\n   *\n   *\n  * *\n*     *");
+        }
+    }
+}
