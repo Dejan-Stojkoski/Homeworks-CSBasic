45e1057ab3cc2f57cdc54dfacc74c8af14ef3ccb
Dejan-Stojkoski <dejan_pp2005@live.com>
2021-04-02 21:05:37 +0200

    Bonus Homework Class4 Upload

---
+++ Class 4/BonusHomework/CharactersGame/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace CharactersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a full sentence: ");
+                string sentence = Console.ReadLine();
+
+                Console.Write("Enter a single character: ");
+                string characterString = Console.ReadLine();
+
+                bool validCharParse = char.TryParse(characterString, out char character);
+
+                if(validCharParse && sentence.Length > 0)
+                {
+                    FindCharacter(sentence, character);
+
+                    while (true)
+                    {
+                        Console.Write("\nEnter a number: ");
+                        string numberString = Console.ReadLine();
+
+                        bool validNumberParse = int.TryParse(numberString, out int number);
+
+                        if (validNumberParse && number > 0)
+                        {
+                            Substring(sentence, number);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You can only enter positive integer numbers! Try again!");
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You have not entered a sentence or your character input is invalid! Try again!");
+                    continue;
+                }
+            }
+        }
+
+        static void FindCharacter(string sentence, char character)
+        {
+            string sentenceToLower = sentence.ToLower();
+            char toLowerChar = char.ToLower(character);
+            int count = 0;
+
+            char[] charArray = sentenceToLower.ToCharArray();
+            foreach(char item in charArray)
+            {
+                if(item == toLowerChar)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($@"The character ""{character}"" in the given sentence is found {count} times.");
+        }
+
+        static void Substring(string sentence, int number)
+        {
+            if (number > sentence.Length) Console.WriteLine($"There are only {sentence.Length} characters in the string!");
+            else
+            {
+                string resultString = sentence.Substring(0, number);
+                Console.WriteLine($@"The substring is: ""{resultString}""");
+            }
+        }
+    }
+}
---
+++ Class 4/BonusHomework/FunWithStrings/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace FunWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string mainString = Console.ReadLine();
+
+            FunWithStrings(mainString);
+        }
+
+        static void FunWithStrings(string mainString)
+        {
+            char[] charString = mainString.ToCharArray();
+            Array.Reverse(charString);
+            string reversedString = new string(charString);
+
+            mainString.ToLower();
+            int countVowels = 0;
+            foreach(char item in charString)
+            {
+                if(item == 'a' || item == 'o' || item == 'i' || item == 'u' || item == 'e')
+                {
+                    countVowels++;
+                }
+            }
+
+            string[] splitedString = mainString.Split(" ");
+            string largestWord = splitedString[0];
+            string smallestWord = splitedString[0];
+            int wordsCount = 0;
+            foreach (string word in splitedString)
+            {
+                if (word.Length > largestWord.Length) largestWord = word;
+                if (word.Length < smallestWord.Length) smallestWord = word;
+                if (word.Length > 0 && word != " ") wordsCount++;
+            }
+
+
+
+            Console.WriteLine($"\nThis is the reversed string: {reversedString}");
+            Console.WriteLine($"There were {countVowels} vowels found!");
+            Console.WriteLine($"Is the string palindrome? {mainString == reversedString}");
+            Console.WriteLine($@"The largest word is: ""{largestWord}""");
+            Console.WriteLine($@"The smallest word is: ""{smallestWord}""");
+            Console.WriteLine($@"The string have {wordsCount} words!");
+            FindTheChar(mainString);
+
+        }
+
+        static void FindTheChar(string mainString)
+        {
+            int[] charCount = new int[256];
+            int length = mainString.Length;
+            for (int i = 0; i < length; i++)
+            {
+                charCount[mainString[i]]++;
+            }
+            int maxCount = -1;
+            char character = ' ';
+            for (int i = 0; i < length; i++)
+            {
+                if (maxCount < charCount[mainString[i]])
+                {
+                    maxCount = charCount[mainString[i]];
+                    character = mainString[i];
+                }
+            }
+            Console.WriteLine($@"The most used character is ""{character}"" and it appears {maxCount} times.");
+        }
+    }
+}
